753df952c1456c67e67b0819fc1b26aabb7726f8
Uriel Barbosa <uprieto@gmail.com>
2024-10-08 20:54:12 -0300

    feat: add object first instance and call the atacar method

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 class Personagem {
-  constructor(nome, idade, classe, etnia) {
+  constructor(nome, idade, classe, especie) {
     this.nome = nome;
     this.idade = idade;
     this.classe = classe;
-    this.etnia = etnia;
+    this.especie = especie;
   }
 
   atacar(ataque) {
-    console.log(`O ${this.classe} atacou usando ${ataque}`);
+    console.log(`${this.nome}, o ${this.classe} atacou usando ${ataque}`);
   }
 }
+
+const primeiroPersonagem = new Personagem("Olaf", 43, "Bárbaro", "Humano");
+primeiroPersonagem.atacar("Martelo de Guerra");
